ed5a3cc1949f6471e9d7c282b9444caaa38652a1
Oscar Espinosa <oscarzx@gmail.com>
2023-02-13 15:59:03 -0500

    Completion endpoint books

--- backend/EcommerceBooks/EcommerceBooks.API/Routes/BooksRoutes.cs
+++ backend/EcommerceBooks/EcommerceBooks.API/Routes/BooksRoutes.cs
@@ -9,8 +9,8 @@ public static class BooksRoutes
         app.MapGet("/books", GetBooks).WithTags("Books");
         app.MapGet("/books/{id}", GetBookById).WithTags("Books");
         app.MapPost("/books", PostBook).WithTags("Books");
-        //app.MapPut("/books/{id}", PutBook).WithTags("Books");
-        //app.MapDelete("/books/{id}", DeleteBook).WithTags("Books");
+        app.MapPut("/books/{id}", PutBook).WithTags("Books");
+        app.MapDelete("/books/{id}", DeleteBook).WithTags("Books");
 
         return app;
 
@@ -28,6 +28,46 @@ public static class BooksRoutes
             var book = await repository.PostAsync(newBook.ToBook());
             await repository.SaveChangesAsync();
             return Results.Ok(book.ToBookDTO());
+        }
+
+        async Task<IResult> PutBook(BooksRepository repository, int id, NewBookDTO newBookDTO)
+        {
+            var existingBook = await repository.GetAsync(id);
+
+            if(existingBook is not null)
+            {
+                existingBook.EditionDate= newBookDTO.EditionDate;
+                existingBook.Price= newBookDTO.Price;
+                existingBook.Author= newBookDTO.Author;
+                existingBook.AuthorCountry= newBookDTO.AuthorCountry;
+                existingBook.Editorial= newBookDTO.Editorial;
+                existingBook.Name= newBookDTO.Name;
+                existingBook.PhotoUrl= newBookDTO.PhotoUrl;
+
+                await repository.PutAsync(existingBook);
+                await repository.SaveChangesAsync();
+                return Results.Ok();
+            }
+            else
+            {
+                return Results.NotFound();
+            }
+        }
+
+        async Task<IResult> DeleteBook(BooksRepository repository, int id)
+        {
+            var existingBook = await repository.GetAsync(id);
+
+            if(existingBook is not null)
+            {
+                await repository.DeleteAsync(id);
+                await repository.SaveChangesAsync();
+                return Results.Ok();
+            }
+            else
+            {
+                return Results.NotFound();
+            }
         }
     }
 }
