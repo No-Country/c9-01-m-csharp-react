d7d5808abf110444945fc30e0b6cd0327ff66084
Oscar Espinosa <oscarzx@gmail.com>
2023-02-15 00:07:21 -0500

    Add Authentication

---
+++ backend/EcommerceBooks/EcommerceBooks.API/Identity/UserManagement.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace EcommerceBooks.API.Identity;
+
+public class UserManagement
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly JwtOptions _jwtOptions;
+    public UserManagement(UserManager<ApplicationUser> userManager, IOptions<JwtOptions> options)
+        => (_userManager, _jwtOptions) = (userManager,options.Value);
+
+    public async Task<string> GenerateJwtAsync(ApplicationUser applicationUser)
+    {
+        var key = Encoding.ASCII.GetBytes(_jwtOptions.Key);
+        var claims = await _userManager.GetClaimsAsync(applicationUser);
+        claims.Add(new("Id", applicationUser.Id));
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Issuer = _jwtOptions.Issuer,
+            Audience = _jwtOptions.Audience,
+            Expires = DateTime.UtcNow.Add(TimeSpan.FromHours(_jwtOptions.ExpirationInHours)),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+            SecurityAlgorithms.HmacSha256Signature),
+            Subject = new(claims)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
+    public async Task<(bool created, IEnumerable<string> errors)> CreateUserAsync(NewUserDTO user, bool isAdmin)
+    {
+        ApplicationUser newUser = new()
+        {
+            Address = user.Address,
+            UserName = user.UserName,
+            Email = user.Email,
+            PhoneNumber = user.PhoneNumber
+        };
+
+        var created = await _userManager.CreateAsync(newUser, user.Password);
+
+        if (created.Succeeded)
+        {
+            Claim adminClaim = new("IsAdmin", isAdmin.ToString());
+            await _userManager.AddClaimAsync(newUser, adminClaim);
+            return (true, null!);
+        }
+            
+        else
+            return (false, created.Errors.Select(e => e.Description));
+    }
+
+    public async Task<(bool success, ApplicationUser user, string error)> ValidateUserCredentialAsync(LoginCredentialsDTO loginCredentialsDTO)
+    {
+        var applicationUser = await _userManager.FindByNameAsync(loginCredentialsDTO.UserName);
+
+        if(applicationUser is not null)
+        {
+            var succeded = await _userManager.CheckPasswordAsync(applicationUser, loginCredentialsDTO.Password);
+
+            //var success = _userManager.PasswordHasher.VerifyHashedPassword(applicationUser, 
+            //    applicationUser.PasswordHash, loginCredentialsDTO.Password);
+
+            return succeded 
+                ? (true, applicationUser, null!)
+                : (false, null!, "Usuario o contraseña incorrectos");
+        }
+        else
+            return (false, null!, "Usuario o contraseña incorrectos");
+    }
+}
--- backend/EcommerceBooks/EcommerceBooks.API/Program.cs
+++ backend/EcommerceBooks/EcommerceBooks.API/Program.cs
@@ -1,15 +1,83 @@
 using EcommerceBooks.API.Data;
+using EcommerceBooks.API.Identity;
 using EcommerceBooks.API.Repository;
 using EcommerceBooks.API.Routes;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Books API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please insert a JWT with bearer",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    c.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            Array.Empty<string>()
+                        }
+        });
+});
 builder.Services.AddSqlServer<EcommerceBooksContext>(builder.Configuration.GetConnectionString("BooksDB"));
+builder.Services.AddSqlServer<IdentityContext>(builder.Configuration.GetConnectionString("IdentityDB"));
 builder.Services.AddScoped<BooksRepository>();
 builder.Services.AddScoped<BookGenderRepository>();
 builder.Services.AddScoped<GenderRepository>();
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(JwtOptions.Jwt));
+builder.Services.AddScoped<UserManagement>();
+
+
+builder.Services.AddIdentityCore<ApplicationUser>(
+    options =>
+    {
+        options.Password.RequiredLength = 8;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequireDigit = true;
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
+    }
+    ).AddEntityFrameworkStores<IdentityContext>();
+
+JwtOptions jwtOptions = builder.Configuration.GetSection(JwtOptions.Jwt).Get<JwtOptions>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtOptions.Key)),
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateLifetime = true,
+            ValidAudience = jwtOptions.Audience,
+            ValidIssuer = jwtOptions.Issuer
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("IsAdmin", policyBuilder => policyBuilder.RequireClaim("IsAdmin", true.ToString()));
+});
 
 var app = builder.Build();
 
@@ -20,11 +88,17 @@ if (app.Environment.IsDevelopment())
 }
 
 //Registrar todas las rutas
+
 app.AddBooksRoutes();
 app.AddBookGendersRoutes();
 app.AddGendersRoutes();
+app.AddAuthentication();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 
 app.Run();
