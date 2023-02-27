6e6bf5b5ddea9bf153bbed8e4c8904ba20c73601
Isaac Urdaneta <urdanetacuarteisaacdavid@gmail.com>
2023-02-27 19:42:36 -0400

    estamos trabajando

---
+++ frontend/src/DB/autoAyuda.js
@@ -0,0 +1,77 @@
+
+let autoayuda = [
+    {
+        name:"El poder del ahora",
+        Editorial:"FonoLibro",
+        author:"Eckhart Tolle",
+        price:"12.99",
+        editionDate:"1997-02-24T01:51:26.814Z",
+        photoUrl:"https://labibliotecaperdida.com/wp-content/uploads/El-poder-del-ahora.jpg",
+        id:"1"
+    },
+    {
+        name:"El monje que vendió su ferrari",
+        Editorial:"DeBolsillo",
+        author:"Robin Sharma",
+        price:"6.99",
+        editionDate:"1996-02-24T01:51:26.814Z",
+        photoUrl:"https://m.media-amazon.com/images/I/81+Z8UHpn2L._AC_UF1000,1000_QL80_.jpg",
+        id:"2"
+    },
+    {
+        name:"Como hacer buenas cosas",
+        Editorial:"DeBolsillo",
+        author:"Marián Rojas Estapé",
+        price:"10.99",
+        editionDate:"2018-02-24T01:51:26.814Z",
+        photoUrl:"https://www.planetadelibros.com.ve/usuaris/libros/fotos/281/m_libros/280326_portada_como-hacer-que-te-pasen-cosas-buenas_marian-rojas_201807031215.jpg",
+        id:"3"
+    },
+    {
+        name:"El sutil arte de que te importe un carajo",
+        Editorial:"HarperCollins Mexico",
+        author:"Mark Manson",
+        price:"15.99",
+        editionDate:"2016-02-24T01:51:26.814Z",
+        photoUrl:"https://m.media-amazon.com/images/I/51YLYltxn3L.jpg",
+        id:"4"
+    },
+    {
+        name:"Tus Zonas Erroneas",
+        Editorial:"HarperCollins Mexico",
+        author:"Wayne W, Dyer",
+        price:"19.99",
+        editionDate:"1977-02-24T01:51:26.814Z",
+        photoUrl:"https://covers.alibrate.com/b/59872eb9cba2bce50c208d61/599b743c-9b2c-489a-8d20-0f3a4d1d7cc4/share",
+        id:"5"
+    },
+    {
+        name:"El hombre en busca del sentido",
+        Editorial:"Herder",
+        author:"Viktor Frankl",
+        price:"25.99",
+        editionDate:"2004-02-24T01:51:26.814Z",
+        photoUrl:"https://m.media-amazon.com/images/I/81MAQ+fkjrL.jpg",
+        id:"6"
+    },
+    {
+        name:"Encuentra a tu persona vitamina",
+        Editorial:"Espasa",
+        author:"Marián Rojas Estapé",
+        price:"25.99",
+        editionDate:"2021-02-24T01:51:26.814Z",
+        photoUrl:"https://www.planetadelibros.com.ve/usuaris/libros/fotos/331/m_libros/330214_portada_encuentra-tu-persona-vitamina_marian-rojas-estape_202106071658.jpg",
+        id:"7"
+    },
+    {
+        name:"El pequeño gran libro de la ansiedad",
+        Editorial:"bookEt",
+        author:"Ferran Cases",
+        price:"25.99",
+        editionDate:"2020-02-24T01:51:26.814Z",
+        photoUrl:"https://m.media-amazon.com/images/I/71Q6bK5XK4L.jpg",
+        id:"8"
+    },
+];
+
+export default autoayuda;
