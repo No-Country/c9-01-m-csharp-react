451ed7b4a1b1d49b525ebfc72c069e18ca162c01
Jose Pinto <alipinko123@gmail.com>
2023-03-03 00:01:29 -0500

    se hace la logica de las operaciones en el cart

---
+++ frontend/src/Components/CompraRealizada.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CompraRealizada = () => {
+  return (
+    <div className="">
+      <h2>Compra realizada</h2>
+      <h3>Su compra fue procesada con éxito.</h3>
+      <p>Revise su correo electronico hay fue enviado su libro</p>
+      <button>Ir a la biblioteca</button>
+    </div>
+  );
+};
+
+export default CompraRealizada;
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -19,85 +19,76 @@ const Cart = () => {
     setLibrosMostrar([...autoayuda]);
   }, [0]);
 
-  const code = 10;
+  const array = [];
+
+  const value = cart.map((prod) => {
+    array.push(prod.price * prod.quantity);
+  });
+  const codeP = 10;
   //parametros rest
-  const subtotal = (uno, ...parameters) => {
-    return librosMostrar[0].price + librosMostrar[1].price;
+  const subtotal = (array) => {
+    let totall = 0;
+    for (const arr of array) {
+      totall += parseFloat(arr);
+    }
+    return totall;
   };
 
   const total = (codeP) => {
-    const porcentajecode = (subtotal() * codeP) / 100;
-    return subtotal - parseInt(porcentajecode);
+    const porcentajecode = (subtotal(array) * parseFloat(codeP)) / 100;
+    const final = parseFloat(subtotal(array)) - parseFloat(porcentajecode);
+    console.log(porcentajecode);
+    return final;
   };
 
   return (
     <div className="mb-8 flex flex-col justify-center items-center ">
       <Header />
 
-      <h2 cl assName="text-3xl px-8 py-4">
-        Carrito de compras
-      </h2>
+      <h2 className="text-3xl px-8 py-4 pt-8">Carrito de compras</h2>
 
-      <div>
+      <div className="max-w-4xl mb-8">
         {cart.map((prod) => (
-          <div className="flex p-8 gap-4 justify-between max-w-3xl">
-            <div className="flex gap-4">
-              <div className="h-20 w-16">
-                <img src={prod.photoUrl} alt="foto de libro" />
-              </div>
-              <div className="flex flex-col justify-between self-start">
-                <div className="mb-4">
-                  <h3 className="font-semibold text-base">{prod.name}</h3>
-                  <h4 className="font-light text-xs">{prod.author}</h4>
+          <div>
+            <div className="flex p-4 gap-4 justify-between max-w-3xl">
+              <div className="flex gap-4">
+                <div className="h-20 w-16">
+                  <img src={prod.photoUrl} alt="foto de libro" />
                 </div>
-                <div>- {prod.quantity} +</div>
+                <div className="flex flex-col justify-between self-start">
+                  <div className="mb-4">
+                    <h3 className="font-semibold text-base">{prod.name}</h3>
+                    <h4 className="font-light text-xs">{prod.author}</h4>
+                  </div>
+                  <div>- {prod.quantity} +</div>
+                </div>
               </div>
-            </div>
-            <div className="flex flex-col justify-between">
-              <BsTrash />
-              <h3>${prod.price}</h3>
+              <div className="flex flex-col justify-between">
+                <BsTrash />
+                <h3>${prod.price}</h3>
+              </div>
             </div>
           </div>
         ))}
       </div>
-      {/* <div className='max-w-4xl mb-8'>
-            <div className='flex p-8 gap-4 justify-between max-w-3xl'>
-                <div className='flex gap-4'>
-                    <div className='h-20 w-16'>
-                        <img src={prod.photoUrl} alt="foto de libro" />
-                    </div>
-                    <div  className='flex flex-col justify-between self-start'>
-                        <div className='mb-4'>
-                            <h3 className='font-semibold text-base'>{prod.name}</h3> 
-                            <h4 className='font-light text-xs'>{prod.author}</h4>
-                        </div>
-                        <div>
-                        - 01 + 
-                        </div>
-                    </div>
-                </div>
-                <div className='flex flex-col justify-between'>
-                <BsTrash/>
-                    <h3>${prod.price}</h3>
-                </div>
+      <div>
+        {
+          <div className="mb-8">
+            <div className="flex gap-4 justify-between max-w-3xl w-80">
+              <h4>subtotal:</h4>
+              <h4>{subtotal(array)} $</h4>
             </div>
-        
-
-       </div>   
-       <div className='mb-8'> 
-        <div className='flex gap-4 justify-between max-w-3xl w-80'>
-            <h4>subtotal:</h4>
-             <h4>{subtotal()} $</h4> 
-        </div>
-        <div  className='flex gap-4 justify-between max-w-3xl w-80'>
-            <h4>Código promocional</h4>
-            <h4>{code}%</h4>
-        </div>
-        <div className='flex gap-4 justify-between max-w-3xl w-80'>
-            <h4>Total</h4>
-            <h4>17,982 $</h4>
-        </div>
-       </div> */}
+            <div className="flex gap-4 justify-between max-w-3xl w-80">
+              <h4>Código promocional</h4>
+              <h4>{codeP}%</h4>
+            </div>
+            <div className="flex gap-4 justify-between max-w-3xl w-80">
+              <h4>Total</h4>
+              <h4>{total(codeP)} $</h4>
+            </div>
+          </div>
+        }
+      </div>
 
       <NavLink
         to={"/pay"}
